342d722e1c776ee6b6091236ee023de0a3f014d7
ailuoku6-blog <18897514992@163.com>
2020-05-01 22:38:52 +0800

    init project

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        System.out.println(System.getProperty("java.library.path"));
+//        ArrayList<PcapIf> alldevs = new ArrayList<>();
+//        StringBuilder errbuf = new StringBuilder();
+//        int r = Pcap.findAllDevs(alldevs,errbuf);
+//        System.out.println(r==Pcap.NOT_OK);
+//        System.out.println(alldevs.size());
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
